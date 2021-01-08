117dc58cb9181f6e45835dd0a209fa2a85f449c5
michaelChidi <cmichael@consumermfb.com.ng>
2021-01-08 11:21:14 +0100

    Anothe page added

--- src/views/mainprogram.vue
+++ src/views/mainprogram.vue
@@ -4,13 +4,15 @@
     <div class="col-start-2 col-span-11">
       <!-- <program></program> -->
       <program-two></program-two>
+      <!-- <program-details></program-details> -->
     </div>
   </div>
 </template>
 
 <script>
-import Program from "../components/program/program.vue";
+// import Program from "../components/program/program.vue";
 import ProgramTwo from "../components/program/program2.vue";
+// import programDetails from "../components/program/programDetails.vue";
 // @ is an alias to /src
 import sidebar from "../components/sidebar/sidebar.vue";
 
@@ -18,8 +20,9 @@ export default {
   name: "mainprogram",
   components: {
     sidebar,
-    Program,
+    // Program,
     ProgramTwo,
+    // programDetails
   },
 };
 </script>
