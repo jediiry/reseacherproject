0030584b28e2eab74b0654b9e8049463975aab84
jediiry <jediiry@gmail.com>
2021-01-09 13:10:55 +0100

    updated discovery by henry

---
+++ src/components/submission/submission.vue
@@ -0,0 +1,25 @@
+<template src="./submission.html"> </template>
+
+<script>
+import mainheader from "../header/header";
+import noitem from "../noitem/noitem";
+export default {
+  name: "submission",
+  components: {
+    mainheader,
+    noitem,
+  },
+  data() {
+    return {
+      tab: "all",
+    };
+  },
+  methods: {
+    setTab(param) {
+      this.tab = param;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/mainsubmission.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-7 mainfade">
+    <sidebar></sidebar>
+    <div class="col-start-2 col-span-11">
+      <submission></submission>
+    </div>
+  </div>
+</template>
+
+<script>
+import submission from "../components/submission/submission.vue";
+// @ is an alias to /src
+import Sidebar from "../components/sidebar/sidebar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Sidebar,
+    submission,
+  },
+};
+</script>
