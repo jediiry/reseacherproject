e024bbb70be2dde5cd677f98a684c8b201646cc1
jediiry <jediiry@gmail.com>
2021-01-09 14:50:21 +0100

    update logout

--- src/views/mainleadership.vue
+++ src/views/mainleadership.vue
@@ -2,17 +2,15 @@
   <div class="grid grid-cols-7 mainfade">
     <sidebar></sidebar>
     <div class="col-start-2 col-span-11">
-      <!-- <leadership></leadership> -->
-      <leadership-two></leadership-two>
-
+      <leadership></leadership>
+      <!-- <leadership-two></leadership-two> -->
     </div>
   </div>
 </template>
 
 <script>
-
-// import leadership from "../components/leadership/leadership.vue";
-import leadershipTwo from "../components/leadership/leadership2.vue";
+import leadership from "../components/leadership/leadership.vue";
+// import leadershipTwo from "../components/leadership/leadership2.vue";
 
 // @ is an alias to /src
 import sidebar from "../components/sidebar/sidebar.vue";
@@ -21,9 +19,8 @@ export default {
   name: "maindiscovery",
   components: {
     sidebar,
-    // leadership,
-    leadershipTwo,
-
+    leadership,
+    // leadershipTwo,
   },
 };
 </script>
